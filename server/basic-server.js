7455bf2a22858b064841d2024c0123de2537a22d
englundn <englundn@gmail.com>
2016-10-04 12:30:10 -0700

    Working on static file serving.

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,7 +2,17 @@
 var http = require('http');
 
 var requestHandler = require('./request-handler');
-debugger;
+var staticServer = require('node-static');
+var util = require('util');
+
+var webroot = '../client/client/';
+
+
+var file = new(staticServer.Server)(webroot, {
+  cache: 600,
+  headers: {'X-powered-by': 'node-static'}
+});
+
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
--- server/request-handler.js
+++ server/request-handler.js
@@ -20,16 +20,13 @@ var messages = [{
 
 var requestHandler = function(request, response) {
 
-
-  // The outgoing status.
   var statusCode = 404;
 
-  // See the note below about CORS headers.
   var defaultCorsHeaders = {
     'access-control-allow-origin': '*',
     'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'access-control-allow-headers': 'content-type, accept',
-    'access-control-max-age': 10 // Seconds.
+    'access-control-max-age': 10
   };
 
 
@@ -41,50 +38,48 @@ var requestHandler = function(request, response) {
   var path = parsedUrl.pathname;
 
 
-
   headers['Content-Type'] = 'JSON';
   if ( path === '/classes/messages') {
-
     if (method === 'POST') {
-
       statusCode = 201;
-
       request.on('data', function(data) {
-
         messages = messages.concat(JSON.parse(data));
-
       });
     }
     if (method === 'GET' || method === 'OPTIONS') {
       statusCode = 200;
     }
-
   }
-
  
   response.writeHead(statusCode, headers);
 
+  //Allow static file access
+  var fs = require('fs');
+  var index;
+  fs.readFile('/Users/student/Desktop/2016-09-chatterbox-server/client/client/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    response.write(data);
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      results: messages
+    };
 
-  var responseBody = {
-    headers: headers,
-    method: method,
-    url: url,
-    results: messages
-  };
 
 
-  // console.log(messages);
-  response.end(JSON.stringify(responseBody));
+    response.end(JSON.stringify(responseBody));
+  });
+
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
 exports.requestHandler = requestHandler;
+
+
+
+
+
+
